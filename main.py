21df55c9b08f6cf2acc4f7c36ee6d83592875e43
22fi051 <22fi051@ms.dendai.ac.jp>
2024-06-14 10:56:10 +0900

    okikujimsg

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import random
 app = FastAPI()
 
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -18,20 +19,20 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.get("/omikuji")
 def omikuji():
     omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
+        "大吉！素晴らしい幸運が舞い込むでしょう。",
+        "中吉！努力が実を結び、良い結果が待っています。",
+        "小吉！ちょっとした幸運があなたの元にやってきます。",
+        "吉！安定した幸せな日々が続くでしょう。",
+        "半吉! まぁまぁの幸運があなたの元にやってきます。",
+        "末吉！努力が実り始め、良い方向に進む時期です。",
+        "末小吉! ちょっとした幸運があなたの元にやってきます。",
+        "悪いことが起こるかもしれませんが、気を引き締めてください。",
+        "小凶。注意が必要な日です。慎重に行動しましょう。",
+        "大凶。厳しい状況が訪れるかもしれませんが、乗り越えましょう。"
     ]
 
-    return omikuji_list[random.randrange(10)]
+    return {"omikuji": omikuji_list[random.randrange(10)]}
 
 @app.post("/circleArea")
-def circleArea(radius: float):
+async def circleArea(radius: float):
     return radius * radius * 3.14159
